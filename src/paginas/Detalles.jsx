9d11c80ca902f1fc84f5840b0f7f267074621e8b
DaniT15 <122325895+DaniT15@users.noreply.github.com>
2025-03-22 19:36:37 -0400

    Detalles

---
+++ src/paginas/Detalles.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../estilos/detalles.css'; // Import the new CSS file
+
+const Detalles = () => {
+
+    return ( 
+
+        <div className='margen'>
+            <div className="div-reservar">
+                <h1>¡Reserva completada exitosamente!</h1>
+                <p>Tu reserva ha sido realizada con éxito. ¡Gracias por elegirnos!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Detalles;
