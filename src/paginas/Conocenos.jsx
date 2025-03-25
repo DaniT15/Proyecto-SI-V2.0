6ea126bc4e6e297d958c18215aebba031c562163
Alma Fuenmayor <alma.fuenmayor@correo.unimet.edu.ve>
2025-03-24 20:43:09 -0400

    mejora conocenos

--- src/paginas/Conocenos.jsx
+++ src/paginas/Conocenos.jsx
@@ -1,13 +1,18 @@
-import '../estilos/conocenos.css'
-import logo from '../assets/logo-conocenos.png'
+import '../estilos/conocenos.css';
+import logo from '../assets/logo-conocenos.png';
 
 export default function Conocenos () {
     return (
         <div>
             <div className='bg'>
-                
                 <h1>CONÓCENOS</h1>  
             </div>
+
+            {/* Logo centrado debajo del banner */}
+            <div className="logo-container">
+                <img src={logo} className="logo-conocenos" />
+            </div>
+
             <div className='div-info'>
                 <div className='info bloque1'>
                     <h1>Apasionados por el Ávila</h1>
@@ -18,15 +23,12 @@ export default function Conocenos () {
                     <h1>¿Qué hacemos?</h1>
                     <p>Ofrecemos rutas guiadas de senderismo en el Ávila de todos los niveles, desde caminatas suaves para principiantes hasta ascensos desafiantes para expertos. También organizamos excursiones personalizadas y campamentos en la montaña, para que puedas disfrutar del Ávila a tu manera.</p>
                 </div>
+
                 <div className='info bloque3'>
                     <h1>¿Por qué lo hacemos?</h1>
                     <p>Porque amamos el Ávila y queremos que otros también lo hagan y la disfruten. Por eso queremos mostrarte los secretos de esta montaña, sus paisajes increíbles, su flora y fauna únicas. Queremos que sientas la emoción de llegar a la cima, la paz de caminar entre los árboles, la conexión con la naturaleza que solo el senderismo puede darte. Queremos inspirarte a cuidar el Ávila, a protegerlo, a valorarlo como el tesoro que es.</p>
                 </div>
-                <div>
-                    <img src={logo} className='logo-conocenos' />
-                </div>
             </div>
-            
         </div>
-    )
+    );
 }
